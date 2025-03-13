2edf675acb92faad626b0f1eef1b416b696459d6
Sadik Boujaida <ibouja@gmail.com>
2025-03-13 01:16:17 +0000

    modified:   _posts/2025-03-12-sujet-02.md 	modified:   _site/404.html 	modified:   _site/about/index.html 	modified:   _site/archives/index.html 	modified:   _site/assets/js/data/search.json 	modified:   _site/assets/js/data/swconf.js 	modified:   _site/categories/cours/index.html 	modified:   _site/categories/exercices/index.html 	modified:   _site/categories/index.html 	modified:   "_site/categories/pr\303\251paration/index.html" 	modified:   _site/categories/python/index.html 	modified:   _site/feed.xml 	modified:   _site/index.html 	modified:   _site/posts/eqdiff/index.html 	modified:   _site/posts/preparation/index.html 	modified:   _site/posts/probas/index.html 	modified:   _site/posts/sujet-01/index.html 	modified:   _site/posts/sujet-02/index.html 	modified:   _site/posts/sujet-03/index.html 	modified:   _site/sitemap.xml 	modified:   _site/tags/analyse/index.html 	modified:   _site/tags/centrale/index.html 	modified:   _site/tags/concours/index.html 	modified:   _site/tags/edl/index.html 	modified:   _site/tags/eqdiff/index.html 	modified:   _site/tags/fctgen/index.html 	modified:   _site/tags/foncgen/index.html 	modified:   _site/tags/index.html 	modified:   _site/tags/int/index.html 	modified:   _site/tags/intparam/index.html 	modified:   _site/tags/marche/index.html 	modified:   _site/tags/mines/index.html 	modified:   _site/tags/mp/index.html 	modified:   _site/tags/proba/index.html 	modified:   _site/tags/probas/index.html 	modified:   _site/tags/serent/index.html 	modified:   _site/tags/sujet/index.html 	modified:   _site/tags/vad/index.html 	modified:   _site/tags/xens/index.html

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741828401',resources: [
+    cacheName: 'chirpy-1741828565',resources: [
       '/cpgem/assets/css/jekyll-theme-chirpy.css',
       '/cpgem/',
       
