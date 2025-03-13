a1e88cd0d34f17c1ab4e5834a4395bcd30df1b05
Sadik Boujaida <ibouja@gmail.com>
2025-03-13 00:27:42 +0000

    modified:   _config.yml 	modified:   _site/about/index.html 	modified:   _site/archives/index.html 	modified:   _site/assets/js/data/search.json 	modified:   _site/assets/js/data/swconf.js 	modified:   _site/categories/index.html 	modified:   _site/feed.xml 	modified:   _site/posts/sujet-01/index.html 	modified:   _site/sitemap.xml 	modified:   _site/tags/index.html

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741825005',resources: [
+    cacheName: 'chirpy-1741825563',resources: [
       '/cpgem/assets/css/jekyll-theme-chirpy.css',
       '/cpgem/',
       
