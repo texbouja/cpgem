294fc5ec117186f319e86fd2c2ae1a96a650c752
Sadik Boujaida <ibouja@gmail.com>
2025-03-14 03:03:16 +0000

    deleted:    _posts/2025-03-10-eqdiff.md 	new file:   _posts/cours/2025-03-10-eqdiff.md 	renamed:    _posts/2025-03-10-probas.md -> _posts/cours/2025-03-10-probas.md 	new file:   _posts/preparation2025/2025-03-13-sujet-04.md 	modified:   _site/404.html 	modified:   _site/about/index.html 	modified:   _site/archives/index.html 	modified:   _site/assets/js/data/search.json 	modified:   _site/assets/js/data/swconf.js 	new file:   _site/assets/pdf/centrale2018mp2c.pdf 	new file:   _site/assets/pdf/centrale2018mp2e.pdf 	new file:   _site/assets/pdf/centrale2018mp2r.pdf 	new file:   _site/assets/pdf/rapCS2018MP_0.pdf 	modified:   _site/categories/cours/index.html 	modified:   _site/categories/exercices/index.html 	modified:   _site/categories/index.html 	modified:   "_site/categories/pr\303\251paration/index.html" 	modified:   _site/categories/python/index.html 	modified:   _site/feed.xml 	modified:   _site/index.html 	modified:   _site/posts/eqdiff/index.html 	modified:   _site/posts/preparation/index.html 	modified:   _site/posts/probas/index.html 	modified:   _site/posts/sujet-01/index.html 	modified:   _site/posts/sujet-02/index.html 	modified:   _site/posts/sujet-03/index.html 	new file:   _site/posts/sujet-04/index.html 	modified:   _site/sitemap.xml 	modified:   _site/tags/analyse/index.html 	modified:   _site/tags/calcdiff/index.html 	new file:   _site/tags/centrale/index.html 	modified:   _site/tags/concours/index.html 	modified:   _site/tags/edl/index.html 	modified:   _site/tags/eqdiff/index.html 	modified:   _site/tags/expmat/index.html 	modified:   _site/tags/fctgen/index.html 	modified:   _site/tags/foncgen/index.html 	modified:   _site/tags/index.html 	modified:   _site/tags/int/index.html 	modified:   _site/tags/intparam/index.html 	modified:   _site/tags/marche/index.html 	modified:   _site/tags/mines/index.html 	modified:   _site/tags/mp/index.html 	modified:   _site/tags/proba/index.html 	modified:   _site/tags/probas/index.html 	modified:   _site/tags/reduc/index.html 	modified:   _site/tags/serent/index.html 	modified:   _site/tags/vad/index.html 	modified:   _site/tags/xens/index.html 	new file:   assets/pdf/centrale2018mp2c.pdf 	new file:   assets/pdf/centrale2018mp2e.pdf 	new file:   assets/pdf/centrale2018mp2r.pdf 	new file:   assets/pdf/rapCS2018MP_0.pdf

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741894804',resources: [
+    cacheName: 'chirpy-1741921208',resources: [
       '/cpgem/assets/css/jekyll-theme-chirpy.css',
       '/cpgem/',
       
