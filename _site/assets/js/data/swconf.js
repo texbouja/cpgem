30089160d9e8206d1def3528bb60d5f5f1ff34ec
Sadik Boujaida <ibouja@gmail.com>
2025-03-14 03:17:38 +0000

    modified:   _posts/preparation2025/2025-03-13-sujet-04.md 	modified:   _site/about/index.html 	modified:   _site/archives/index.html 	modified:   _site/assets/js/data/swconf.js 	modified:   _site/categories/index.html 	modified:   _site/feed.xml 	modified:   _site/posts/sujet-04/index.html 	modified:   _site/sitemap.xml 	modified:   _site/tags/index.html

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741921208',resources: [
+    cacheName: 'chirpy-1741922242',resources: [
       '/cpgem/assets/css/jekyll-theme-chirpy.css',
       '/cpgem/',
       
