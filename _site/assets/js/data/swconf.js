8d49daa721f63d3829e4b19e456b07bf6a38e056
Sadik Boujaida <ibouja@gmail.com>
2025-03-16 23:03:21 +0000

    new file:   _posts/preparation2025/2025-03-16-polorth.md 	modified:   _site/404.html 	modified:   _site/about/index.html 	modified:   _site/archives/index.html 	modified:   _site/assets/js/data/search.json 	modified:   _site/assets/js/data/swconf.js 	new file:   _site/assets/pdf/polorth_phone.pdf 	new file:   _site/assets/pdf/polorth_print.pdf 	new file:   _site/assets/pdf/polorth_tablet.pdf 	new file:   _site/categories/concentration/index.html 	modified:   _site/categories/cours/index.html 	modified:   _site/categories/exercices/index.html 	modified:   _site/categories/index.html 	modified:   "_site/categories/pr\303\251paration/index.html" 	modified:   _site/categories/python/index.html 	modified:   _site/feed.xml 	modified:   _site/index.html 	modified:   _site/posts/eqdiff/index.html 	new file:   _site/posts/polorth/index.html 	modified:   _site/posts/preparation/index.html 	modified:   _site/posts/probas/index.html 	modified:   _site/posts/sujet-01/index.html 	modified:   _site/posts/sujet-02/index.html 	modified:   _site/posts/sujet-03/index.html 	modified:   _site/posts/sujet-04/index.html 	modified:   _site/sitemap.xml 	new file:   _site/tags/alglin/index.html 	modified:   _site/tags/analyse/index.html 	modified:   _site/tags/calcdiff/index.html 	modified:   _site/tags/centrale/index.html 	modified:   _site/tags/concours/index.html 	modified:   _site/tags/edl/index.html 	modified:   _site/tags/eqdiff/index.html 	new file:   _site/tags/espreh/index.html 	modified:   _site/tags/expmat/index.html 	modified:   _site/tags/fctgen/index.html 	modified:   _site/tags/foncgen/index.html 	modified:   _site/tags/index.html 	modified:   _site/tags/int/index.html 	modified:   _site/tags/intparam/index.html 	modified:   _site/tags/marche/index.html 	new file:   _site/tags/matpos/index.html 	modified:   _site/tags/mines/index.html 	modified:   _site/tags/mp/index.html 	new file:   _site/tags/polorth/index.html 	modified:   _site/tags/proba/index.html 	modified:   _site/tags/probas/index.html 	modified:   _site/tags/reduc/index.html 	modified:   _site/tags/serent/index.html 	new file:   _site/tags/synth/index.html 	modified:   _site/tags/vad/index.html 	modified:   _site/tags/xens/index.html 	new file:   assets/pdf/polorth_phone.pdf 	new file:   assets/pdf/polorth_print.pdf 	new file:   assets/pdf/polorth_tablet.pdf

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741943877',resources: [
+    cacheName: 'chirpy-1742166099',resources: [
       '/cpgem/assets/css/jekyll-theme-chirpy.css',
       '/cpgem/',
       
