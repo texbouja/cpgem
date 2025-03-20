a67e275eb7261d8b7b4505a2a322f9fb002d2fbf
Sadik Boujaida <ibouja@gmail.com>
2025-03-20 22:32:25 +0000

    modified:   _posts/2025-03-20-expmat.md 	modified:   _site/about/index.html 	modified:   _site/archives/index.html 	modified:   _site/assets/js/data/search.json 	modified:   _site/assets/js/data/swconf.js 	modified:   _site/assets/pdf/expmat_phone.pdf 	modified:   _site/assets/pdf/expmat_print.pdf 	modified:   _site/assets/pdf/expmat_tablet.pdf 	modified:   _site/categories/index.html 	modified:   _site/feed.xml 	modified:   _site/posts/expmat/index.html 	modified:   _site/sitemap.xml 	modified:   _site/tags/index.html 	modified:   assets/pdf/expmat_phone.pdf 	modified:   assets/pdf/expmat_print.pdf 	modified:   assets/pdf/expmat_tablet.pdf

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742441426',resources: [
+    cacheName: 'chirpy-1742509895',resources: [
       '/cpgem/assets/css/jekyll-theme-chirpy.css',
       '/cpgem/',
       
